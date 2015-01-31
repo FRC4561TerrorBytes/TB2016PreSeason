384fcdfb3899dfd331dc60d399744ada96ab26a7
Alek Lewis <Alek Lewis@172.17.2.156>
2015-01-31 15:27:50 -0500

    Added ClawRelease

---
+++ robot_2015/src/org/usfirst/frc/team4561/robot/commands/ClawRelease.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team4561.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team4561.robot.Robot;
+
+
+
+public class ClawRelease extends Command {
+	
+	public ClawRelease() {
+		setTimeout(2);
+		requires(Robot.claw);
+	}
+
+	@Override
+	protected void initialize() {
+    	
+		
+	}
+
+	@Override
+	protected void execute() {
+		Robot.claw.closeClaw();
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return isTimedOut();
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		Robot.claw.stop();
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		end();
+	}
+
+}
