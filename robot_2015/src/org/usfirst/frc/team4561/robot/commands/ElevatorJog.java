e0f58679c2cae2e8748938e306e80b9943498264
Steve <sfwasleski@att.net>
2015-02-21 15:44:01 -0500

    Restore PID specific commands. Not wired up yet. Need to take care to only wire at runtime if the PID subsystem in actually created.

--- robot_2015/src/org/usfirst/frc/team4561/robot/commands/ElevatorJog.java
+++ robot_2015/src/org/usfirst/frc/team4561/robot/commands/ElevatorJog.java
@@ -1,45 +1,48 @@
-//package org.usfirst.frc.team4561.robot.commands;
-//
-//import org.usfirst.frc.team4561.robot.Robot;
-//import edu.wpi.first.wpilibj.command.Command;
-//
-///**
-// *
-// */
-//public class ElevatorJog extends Command {
-//	boolean up;
-//    public ElevatorJog(boolean up) {
-//    	requires(Robot.elevator);
-//    	this.up = up;
-//        // Use requires() here to declare subsystem dependencies
-//        // eg. requires(chassis);
-//    }
-//
-//    // Called just before this Command runs the first time
-//    protected void initialize() {
-//    	if(up) {
-//    		Robot.elevator.jogUp();
-//    	}
-//    	else {
-//    		Robot.elevator.jogDown();
-//    	}
-//    }
-//
-//    // Called repeatedly when this Command is scheduled to run
-//    protected void execute() {
-//    }
-//
-//    // Make this return true when this Command no longer needs to run execute()
-//    protected boolean isFinished() {
-//        return true;
-//    }
-//
-//    // Called once after isFinished returns true
-//    protected void end() {
-//    }
-//
-//    // Called when another command which requires one or more of the same
-//    // subsystems is scheduled to run
-//    protected void interrupted() {
-//    }
-//}
+package org.usfirst.frc.team4561.robot.commands;
+
+import org.usfirst.frc.team4561.robot.Robot;
+import org.usfirst.frc.team4561.robot.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ElevatorJog extends Command {
+	boolean up;
+    public ElevatorJog(boolean up) {
+    	requires((Subsystem) Robot.commonElevator);
+    	this.up = up;
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	if(up) {
+    		((Elevator)Robot.commonElevator).jogUp();
+    	}
+    	else {
+    		((Elevator)Robot.commonElevator).jogDown();
+    	}
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
