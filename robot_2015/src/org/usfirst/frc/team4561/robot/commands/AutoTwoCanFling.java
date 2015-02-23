9db15da648e287c4b7d07873a6d1d261aefaf71e
Dinokaiz2 <kakhtar@rthighschool.org>
2015-02-23 18:23:00 -0500

    5 Automodes added. Implements the tape sensor. Resets encoders after each movement.

---
+++ robot_2015/src/org/usfirst/frc/team4561/robot/commands/AutoTwoCanFling.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team4561.robot.commands;
+
+
+/**
+ *
+ */
+public class AutoTwoCanFling extends Abstract4561AutomodeGroup {
+
+    public  AutoTwoCanFling() {
+
+    	//Rotate: extender faces step
+    	addSequential(new RotateTo(180.0));
+    	//Drive Forward until hit totes, back up a bit if didn't see tape
+    	addSequential(new AutoCardinalFieldRelativeDrive(1, 28, 180, 8));
+    	//Strafe to "hook" easternmost can.
+    	addSequential(new AutoCardinalFieldRelativeDrive(2, 25, 180));
+    	//Pull first can back.
+    	addSequential(new AutoCardinalFieldRelativeDrive(3, 22, 180));
+    	//Strafe to gap between first and second bins
+    	addSequential(new AutoCardinalFieldRelativeDrive(4, 25, 180));
+    	//Drive Forward until hit totes, back up a bit if didn't see tape
+    	addSequential(new AutoCardinalFieldRelativeDrive(1, 22, 180, 8));
+    	//Strafe to second can
+    	addSequential(new AutoCardinalFieldRelativeDrive(4, 25, 180));
+    	//Pull second can back
+    	addSequential(new AutoCardinalFieldRelativeDrive(3, 22, 180));
+    	//Drive to autozone while flinging
+    	addSequential(new AutoCardinalFieldRelativeDrive(3, 48.5, 90));
+    }
+    @Override
+	public double getStartAngle() {
+		// Starts with the front of the robot facing the right wall.
+		return 90.0;
+	}
+}
