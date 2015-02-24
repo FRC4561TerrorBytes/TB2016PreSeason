30f5e262760b91ff4ead2eec5213eceae55ca9af
Steve <sfwasleski@att.net>
2015-02-24 18:30:04 -0500

    Change gyro reset to whenActive and put a min power on the auto cardinal drive commands.

--- robot_2015/src/org/usfirst/frc/team4561/robot/commands/AutoCardinalRobotRelativeDrive.java
+++ robot_2015/src/org/usfirst/frc/team4561/robot/commands/AutoCardinalRobotRelativeDrive.java
@@ -8,6 +8,8 @@ import org.usfirst.frc.team4561.robot.Robot;
  */
 public class AutoCardinalRobotRelativeDrive extends PIDCommand {
 	
+	private static final double MIN_ABSOLUTE_POWER = 0.1;
+	
 	int direction;
 	double inches;
 	/**
@@ -73,6 +75,12 @@ public class AutoCardinalRobotRelativeDrive extends PIDCommand {
 	@Override
 	protected void usePIDOutput(double output) {
 		double motorPower = output;
+		if (Math.abs(output) < MIN_ABSOLUTE_POWER) {
+			if (output > 0)
+				motorPower = MIN_ABSOLUTE_POWER;
+			else
+				motorPower = -MIN_ABSOLUTE_POWER;
+		}
 		//North
 		if(direction == 1) {
 			Robot.driveTrain.driveRobotRelative(0, motorPower);
