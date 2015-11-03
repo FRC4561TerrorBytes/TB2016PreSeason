4365b6eba4ad9cdc8a6c3586895296b478d84cbe
Dinokaiz2 <kakhtar@rthighschool.org>
2015-11-03 17:55:20 -0500

    UNTESTED: Accident, small commit

--- robot_2015/src/org/usfirst/frc/team4561/robot/commands/ArcadeDrive.java
+++ robot_2015/src/org/usfirst/frc/team4561/robot/commands/ArcadeDrive.java
@@ -23,11 +23,14 @@ public class ArcadeDrive extends Command {
     					 			 Robot.oi.getRot());
     }
 
+    
+    
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
 
+    
     // Called once after isFinished returns true
     protected void end() {
     	Robot.driveTrain.stop();
