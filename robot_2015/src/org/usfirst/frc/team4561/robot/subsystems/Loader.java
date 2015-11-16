5fb565a7669e213face04c98dba37ae655398e15
alexk <alexk@172.17.4.170>
2015-11-16 18:41:26 -0500

    Alex Brown UNTESTED,needs placerholder value filled

--- robot_2015/src/org/usfirst/frc/team4561/robot/subsystems/Loader.java
+++ robot_2015/src/org/usfirst/frc/team4561/robot/subsystems/Loader.java
@@ -1,14 +1,17 @@
 package org.usfirst.frc.team4561.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DigitalOutput;
 
 /**
  * Takes a Boolean value.  Turns the loader belt on or off.  
  */
 public class Loader extends Subsystem {
     
-	Victor beltMotor = ;//new(Victor.) //TBD Need to add port to victor
+	Victor beltMotor = new Victor(0); //TO-DO Need to add port to victor
+	DigitalOutput LED = new DigitalOutput(2);
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
@@ -18,7 +21,7 @@ public class Loader extends Subsystem {
     }
     public void moveBelt(boolean x){
     	if(x==true){
-    		beltMotor.set(speed);//TBD need to add speed
+    		beltMotor.set(1);//TO-DO need to add speed
     	}
     	else if(x==false){
     		beltMotor.set(0);
